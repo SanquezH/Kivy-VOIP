1e9e1a585d258d653fad4fca4e398e6f8b504ebf
SanquezH <135459789+SanquezH@users.noreply.github.com>
2024-11-28 19:08:13 -0600

    Added files via upload
    Consists of the files utilized to demonstrate the usage of the Kivy VOIP Module.

---
+++ main.py
@@ -0,0 +1,55 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+import threading
+from voip import Client  # Import from voip.py module
+
+class VOIPClientApp(App):
+    # Initialize a client
+    client = Client()
+
+    # Configure connection from client to VOIP server
+    client.dst_address = "192.168.1.67"  # Set to your server's IP address. Use root domain if using SSL (loopback by default)
+    client.dst_port = 8080  # Set to your server's assigned port (port 8080 by default)
+    client.ssl = False  # Determines if SSL/TLS will be used (False by default)
+    client.tls_version = "TLSv1.3"  # If using SSL, TLSv1.3 is enforced by default
+    client.client_id = "user@kivy.org"  # Supports identifying/authorizing connection (optional)
+    client.debug = True  # Enables print statements for troubleshooting purposes with adb
+    client.timeout = 3  # Sets wait time to connect to server (5 seconds is default)
+    
+    def build(self):
+        # Create a call and end call button to alternate between to allow client control over VOIP call
+        self.layout = BoxLayout(orientation='vertical')
+        self.call_button = Button(text="Call")
+        self.end_call_button = Button(text="End Call", disabled=True)
+        self.call_button.bind(on_press=self.start_call)
+        self.end_call_button.bind(on_press=self.end_call)
+        self.layout.add_widget(self.call_button)
+        self.layout.add_widget(self.end_call_button)
+        return self.layout
+    
+    def auto_end_call(self):  # Automate ending call, including if connection closes externally
+        if self.client.connected and self.client.hasPermission:
+            # If client is connected and has permission, call is active
+            while self.client.active_call:  # Loop that runs until call ends
+                pass
+        if not self.end_call_button.disabled:
+            instance = VOIPClientApp()
+            self.end_call(instance)
+
+    def start_call(self, instance):
+	    # Disable call button after call button is pressed
+        self.call_button.disabled = True
+        self.end_call_button.disabled = False
+        self.client.start_call()  # Initiate the VOIP call
+        self.track_call_thread = threading.Thread(target=self.auto_end_call, daemon=True)
+        self.track_call_thread.start()
+
+    def end_call(self, instance):
+        # Disable end call button after end call button is pressed
+        self.end_call_button.disabled = True
+        self.call_button.disabled = False
+        self.client.end_call()  # End the VOIP call
+        
+if __name__ == "__main__":
+    VOIPClientApp().run()
